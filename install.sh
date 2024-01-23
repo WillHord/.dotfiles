6aaaf0cdac14b282fc82db1b99c8aa5e9e8d5122
Will Hord <whord@ucsc.edu>
2024-01-23 16:01:30 -0500

    adding zshrc and logic for private files

--- install.sh
+++ install.sh
@@ -15,8 +15,13 @@ SH="${HOME}/.bashrc"
 ZSHRC="${HOME}/.zshrc"
 if [ -f "$ZSHRC" ]; then
 	SH="$ZSHRC"
+  if ask "Do you want to install .zshrc?"; then
+    ln -s "$(realpath ".zshrc")" ~/.zshrc
 fi
 
+
+echo "# ====================== Custom Files ============================= #" >> $SH
+echo "# ====================== Custom Files ============================= #"
 echo "Do you want $SH to source: "
 for file in shell/*; do 
   if [ -f "$file" ]; then
@@ -27,6 +32,17 @@ for file in shell/*; do
   fi 
 done
 
+echo "# ====================== Private Files ============================= #" >> $SH
+echo "# ====================== Private Files ============================= #"
+echo "Do you want to source: "
+for file in private/*; do 
+  if [ -f "$file" ]; then
+    filename = $(basename "$file")
+    if ask "${filename}?"; then
+      echo "source $(realpath "$file")" >> "$SH"
+    fi 
+  fi 
+done
 
 # Config Files
 if ask "Do you want to install .tmux.conf?"; then
@@ -36,6 +52,4 @@ fi
 if ask "Do you want to install .vimrc?"; then
   ln -s "$(realpath ".vimrc")" ~/.vimrc 
 fi 
-
-
 
